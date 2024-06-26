81cb179f9b556bd2b122831309b02ff5d4bf8345
automation-test <netanelbenda7@gmail.com>
2024-06-26 11:59:08 +0300

    add new class

---
+++ automation/src/main/java/org/example/openBrowser.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class openBrowser {
+    public static void main(String[] args) {
+        // System Property for Chrome Driver
+        WebDriverManager.chromedriver().setup();
+        // Instantiate a ChromeDriver class.
+        WebDriver driver=new ChromeDriver();
+
+        // Launch Website
+        driver.navigate().to("http://www.javatpoint.com/");
+
+        //Maximize the browser
+        driver.manage().window().maximize();
+
+
+        // Click on the Search button
+        driver.findElement(By.linkText("Core Java")).click();
+
+    }
+}
